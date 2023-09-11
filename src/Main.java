87d9d60c994b56045c0589cdb850ed2428fa93bc
Teemu eemeli Tupola <Teemu.tupola@student.hamk.fi>
2023-09-11 09:17:00 +0300

    tehtävä:D
    Tehtävä olikin niin helppo, etä meni yhdellä kerralla valmiiksi:D

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		String Vastaus;
+		int i = 0;
+	do {
+System.out.println("Quess my name!");
+		Vastaus = in.nextLine();
+		 i++;
+		if(Vastaus.equalsIgnoreCase("Mikko")) {
+			
+			System.out.println("Arvasit oikein! " + i + "- Kerralla!");
+			break;
+		}
+	
+	}while (!Vastaus.equals("loppu"));
+	 if(Vastaus.equals("loppu"))
+	{
+		 i--;
+		System.out.println("Lopetit Pelin " + i + " Kerralla, Parempi onni ensi kerralla");
+	
+		}
 	}
 
 }
